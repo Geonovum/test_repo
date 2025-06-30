868cebd1720016b86751d65de61c726eedc98ab5
pasibun <matthijs_hovestad@hotmail.com>
2025-06-30 20:56:49 +0200

    test

--- js/config.js
+++ js/config.js
@@ -37,8 +37,8 @@ let respecConfig = {
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   publishDate: "2025-06-29",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
-  //publishVersion: "0.0.2",
-  //previousVersion: "0.0.1",
+  publishVersion: "0.0.2",
+  previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   //previousPublishDate: "2014-05-01",
   //previousMaturity: "CV",
